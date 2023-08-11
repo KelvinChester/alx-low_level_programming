792bbeac1b3edba7c8f2516e9993860f67993787
Kelvin Chester <kenechukwuabuonovo@gmail.com>
2023-08-11 12:58:31 -0700

    if whle done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Descibtion: 'get last digit of a no.'
+ * Return: always 0
+ */
+int main(void)
+{
+	int n = 48;
+	int G = 48;
+
+	for (n; n >= 57; n++)
+	{
+		for (g; g >= 57; g++)
+        {
+		if (g == n)
+		{
+			continue;
+		}
+        }
+		putchar(n);
+		putchar(g);
+	}
+	return (0);
+}
